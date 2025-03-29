66b609b4bffe28b82ffc423edd534d59ad2c9ba2
Praveen Patel <praveenpatel5957@gmail.com>
2025-03-30 01:16:15 +0530

    modified:   assets/css/Responsive.css 	modified:   assets/css/style.css 	modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,16 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+
+// Menu
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector("#nav-links");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    
+    // Toggle between menu and close icon
+    menuIcon.classList.toggle("bx-menu");
+    menuIcon.classList.toggle("bx-x");
+});
